5d10b0c9611be9fd62f25ecbeb69c7ddf7bdb1c2
jun-130 <happy.smile.p0p0@gmail.com>
2022-10-04 11:57:22 +0900

    validation(文字数制限)

--- app/models/explanation.rb
+++ app/models/explanation.rb
@@ -1,5 +1,5 @@
 class Explanation < ApplicationRecord
   belongs_to :post
 
-  validates :text, presence: true
+  validates :text, presence: true, length: { maximum: 100 }
 end
--- app/models/introduction.rb
+++ app/models/introduction.rb
@@ -2,4 +2,6 @@ class Introduction < ApplicationRecord
   belongs_to :user
 
   validates :title1, :text1, presence: true
+  validates :title1, :title2, length: { maximum: 40 }
+  validates :text1, :text2, length: { maximum: 250 }
 end
