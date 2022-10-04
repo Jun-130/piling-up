d4397227910d99ff3919ebadf3d207332f0f69a1
jun-130 <happy.smile.p0p0@gmail.com>
2022-10-04 09:14:48 +0900

    画面遷移の修正

--- app/controllers/introductions_controller.rb
+++ app/controllers/introductions_controller.rb
@@ -1,5 +1,5 @@
 class IntroductionsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!
   before_action :move_to_profile_new
   before_action :set_introduction, only: [:edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
