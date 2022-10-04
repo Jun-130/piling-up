adf1173f704c2274a0c2f9c679ba49e371b7fb35
jun-130 <happy.smile.p0p0@gmail.com>
2022-10-04 09:15:57 +0900

    validationの追加

--- app/models/target.rb
+++ app/models/target.rb
@@ -7,4 +7,19 @@ class Target < ApplicationRecord
   end
   validates :completed, inclusion: [true, false],
                         uniqueness: { scope: :user, on: :create, message: '未達成の目標があるため、新しい目標は設定できません' }
+
+  validate :deadline_must_be_in_the_future
+  validate :amount_must_be_more_than_current_savings
+
+  def deadline_must_be_in_the_future
+    if deadline.to_date >= Date.new.next_month
+      errors.add(:deadline, '来月以降の年月を設定してください')
+    end
+  end
+
+  def amount_must_be_more_than_current_savings
+    if amount <= user.current_savings
+      errors.add(:amount, '現在の貯金額より大きい金額を設定してください')
+    end
+  end
 end
